959261ec33455e625e326c6a37fc924e6bec2b0f
Mustafizur Rahman <mustafizur232@gmail.com>
2023-10-02 18:20:09 +0600

    form authentication done

---
+++ js/login.js
@@ -0,0 +1,14 @@
+document.getElementById('btn-submit').addEventListener('click', function () {
+    const emailField = document.getElementById('user-email');
+    const email = emailField.value;
+    // console.log(email);
+    const passwordField = document.getElementById('user-password');
+    const password = passwordField.value;
+    // console.log(email, password);
+    if (email === "mustafizur232@gmail.com" && password === 'chuppu') {
+        console.log('valid user');
+    }
+    else {
+        console.log('Invalid User');
+    }
+})
